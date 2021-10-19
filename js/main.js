53a65d2f879a9dc3e80a4cc57c118e7448b0b338
Adam Nicewarner <nicewa@uw.edu>
2021-10-19 00:36:24 -0700

    struggling with html

--- js/main.js
+++ js/main.js
@@ -11,3 +11,25 @@ function responsive_control() {
     x.className = "topnav";
   }
 }
+
+function myFunction(imgs) {
+  // Get the expanded image
+  var expandImg = document.getElementById("expandedImg");
+  // Get the image text
+  var imgText = document.getElementById("imgtext");
+  // Use the same src in the expanded image as the image being clicked on from the grid
+  expandImg.src = imgs.src;
+  // Use the value of the alt attribute of the clickable image as text inside the expanded image
+  imgText.innerHTML = imgs.alt;
+  // Show the container element (hidden with CSS)
+  expandImg.parentElement.style.display = "block";
+}
+
+function show() {
+  var x = document.getElementById("theCats");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
